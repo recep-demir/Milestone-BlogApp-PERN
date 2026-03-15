e2ae8de9fb5a6ab984e463e211bd68eccca5eb59
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:35:32 +0100

    refactor: streamline category controller methods and improve Swagger documentation

--- server/src/controllers/category.js
+++ server/src/controllers/category.js
@@ -1,105 +1,46 @@
 "use strict"
-
-const Category = require('../models/category');
-const CustomError = require('../helpers/customError');
+const { Category } = require("../models/index");
 
 module.exports = {
-
     list: async (req, res) => {
         /*
             #swagger.tags = ["Categories"]
             #swagger.summary = "List Categories"
-            #swagger.description = `
-                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
-                <ul> Examples:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
-                    <li>URL/?<b>limit=10&page=1</b></li>
-                </ul>
-            `
         */
-
-        const result = await res.getModelList(Category);
-
-        res.status(200).send({
-            error: false,
-            details: await res.getModelListDetails(Category),
-            result
-        });
+        const data = await Category.findAll();
+        res.status(200).send({ error: false, count: data.length, result: data });
     },
-
     create: async (req, res) => {
         /*
             #swagger.tags = ["Categories"]
             #swagger.summary = "Create Category"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                   $ref: "#/definitions/Category"
-                }
-            }
         */
-
-        const result = await Category.create(req.body);
-
-        res.status(201).send({
-            error: false,
-            result
-        });
+        const data = await Category.create(req.body);
+        res.status(201).send({ error: false, result: data });
     },
-
     read: async (req, res) => {
         /*
             #swagger.tags = ["Categories"]
             #swagger.summary = "Get Single Category"
         */
-
-        const result = await Category.findById(req.params.id);
-
-        res.status(200).send({
-            error: false,
-            result
-        });
+        const data = await Category.findByPk(req.params.id);
+        res.status(200).send({ error: false, result: data });
     },
-
     update: async (req, res) => {
         /*
             #swagger.tags = ["Categories"]
             #swagger.summary = "Update Category"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                   $ref: "#/definitions/Category"
-                }
-            }
         */
-
-        const result = await Category.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true });
-
-        if (!result) throw new CustomError("Update failed, data is not found or already updated", 404);
-
-        res.status(202).send({
-            error: false,
-            result
-        });
+        await Category.update(req.body, { where: { id: req.params.id } });
+        const data = await Category.findByPk(req.params.id);
+        res.status(202).send({ error: false, result: data });
     },
-
-    deletee: async (req, res) => {
+    remove: async (req, res) => {
         /*
             #swagger.tags = ["Categories"]
             #swagger.summary = "Delete Category"
         */
-
-        const result = await Category.findByIdAndDelete(req.params.id)
-
-        if (!result) throw new CustomError("Delete failed, data is not found or already deleted", 404);
-
-        res.status(200).send({
-            error: false,
-            result
-        });
+        const data = await Category.destroy({ where: { id: req.params.id } });
+        res.sendStatus(data ? 204 : 404);
     },
-}
+};
