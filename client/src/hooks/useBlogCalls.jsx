c905e759ed6f4c10b88b41e195903a9281423da6
Recep Demir <recep.demir@powercoders.org>
2026-03-16 00:13:42 +0100

    refactor: replace axiosWithToken with direct axios calls and streamline API requests in useBlogCalls hook

--- client/src/hooks/useBlogCalls.jsx
+++ client/src/hooks/useBlogCalls.jsx
@@ -1,107 +1,96 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { addCommentToState, blogSuccess, categorySuccess, commentSuccess, createBlogSuccess, fetchFail, fetchStart,toggleLikeInState } from '../features/blogSlice'
-import { useSelector } from 'react-redux'
-import useAxios from './useAxios'
+import { useDispatch, useSelector } from 'react-redux'
+import { addCommentToState, blogSuccess, categorySuccess, commentSuccess, createBlogSuccess, fetchFail, fetchStart, toggleLikeInState } from '../features/blogSlice'
+import axios from 'axios' // Doğrudan axios kullanacağız, aracı kullanmıyoruz!
 
 const useBlogCalls = () => {
-    const dispatch =useDispatch()
-    const BASE_URL=import.meta.env.VITE_BASE_URL
-    const {token}=useSelector(state => state.auth)
-    const {axiosWithToken}=useAxios()
+    const dispatch = useDispatch()
+    const BASE_URL = import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000/"
+    const { token } = useSelector(state => state.auth)
+
+    // GARANTİ TOKEN FONKSİYONU: Her istekte taze token çeker ve tırnak işaretlerini temizler
+    const getAuthHeader = () => {
+        const rawToken = token || localStorage.getItem("token") || "";
+        const cleanToken = rawToken.replace(/['"]+/g, ''); // Fazladan tırnak varsa temizler
+        return {
+            headers: { Authorization: `Token ${cleanToken}` }
+        };
+    };
 
     const getBlogs = async () => {
         dispatch(fetchStart())
         try {
-            const { data } = await axiosWithToken.get("blogs")           
+            const { data } = await axios.get(`${BASE_URL}blogs`)           
             dispatch(blogSuccess(data))
         } catch (error) {
             dispatch(fetchFail())
         }
     }
-    // const getComments = async () =>{
-    //     dispatch(fetchStart())
-    //     try {
-    //         const { data } = await axiosWithToken.get("comments")
-    //         console.log("cekilen Comments",data);
-    //         dispatch(commentSuccess(data));            
-    //     } catch (error) {
-    //         dispatch(fetchFail())
-    //     }
-    // }
 
     const getCommentsByID = async (blogId) => {
       dispatch(fetchStart());
       try {
-        const { data } = await axiosWithToken.get(
-          `comments?filter[blogId]=${blogId}`
-        );
-        console.log('cekilen Comments', data);
+        const { data } = await axios.get(`${BASE_URL}comments?filter[blogId]=${blogId}`);
         dispatch(commentSuccess(data));
       } catch (error) {
         dispatch(fetchFail());
       }
     };
     
-    const addComment =async (blogId, comment) =>{
+    const addComment = async (blogId, comment) => {
         dispatch(fetchStart());
         try {
-          const { data } = await axiosWithToken.post("comments", { blogId, comment });
-          console.log("giden yorumlar",data)
+          // POST işleminde getAuthHeader()'ı zorla ekliyoruz
+          const { data } = await axios.post(`${BASE_URL}comments`, { blogId, comment }, getAuthHeader());
           dispatch(addCommentToState(data));
         } catch (error) {
-            console.error("Aloo hata var adding comment ", error);
+          console.error("Yorum ekleme hatası:", error);
           dispatch(fetchFail());
         }
-      };
-    const getCategories =async () =>{
+    };
+
+    const getCategories = async () => {
         dispatch(fetchStart())
         try {
-          const { data } = await axiosWithToken.get("categories");
-          console.log("Gelen Kategoriler",data);
+          const { data } = await axios.get(`${BASE_URL}categories`);
           dispatch(categorySuccess(data));
         } catch (error) {
-          console.error("Kategori getirme hatası", error);
           dispatch(fetchFail());
         }
     }
 
-    const addBlog = async(blogData) =>{
+    const addBlog = async (blogData) => {
         dispatch(fetchStart())
-
         try {
-            console.log("Gönderilen Blog Verisi:", blogData)
-            const {data} =await axiosWithToken.post("blogs",blogData)
+            const {data} = await axios.post(`${BASE_URL}blogs`, blogData, getAuthHeader())
             dispatch(createBlogSuccess(data))
-            
         } catch (error) {
-            console.log("Create Blog Error",error)
+            console.log("Create Blog Error:", error)
             dispatch(fetchFail())            
         }
     }
+
     const deleteBlog = async (blogId) => {
         dispatch(fetchStart());
         try {
-          await axiosWithToken.delete(`blogs/${blogId}`);
-          dispatch(getBlogs);
+          await axios.delete(`${BASE_URL}blogs/${blogId}`, getAuthHeader());
+          getBlogs(); // Silince listeyi yenile
         } catch (error) {
           console.error("Delete blog error:", error);
           dispatch(fetchFail());
         }
-      };
-
+    };
 
     const toggleLike = async (id, userId) => {
         try {
-          await axiosWithToken.post(`blogs/${id}/postLike`);
+          await axios.post(`${BASE_URL}blogs/${id}/postLike`, {}, getAuthHeader());
           dispatch(toggleLikeInState({ blogId: id, userId }));
         } catch (error) {
           console.error("Like error:", error);
         }
-      };
+    };
 
-
-  return {getBlogs,toggleLike,addComment,getCategories,addBlog,deleteBlog,getCommentsByID}
+  return { getBlogs, toggleLike, addComment, getCategories, addBlog, deleteBlog, getCommentsByID }
 }
 
 export default useBlogCalls
