bf4770b8940762c0a80df8e3c0f81f4d84a21294
Recep Demir <recep.demir@powercoders.org>
2026-03-16 00:25:49 +0100

    refactor: update key and ID references in MyBlogs component for consistency

--- client/src/pages/MyBlog.jsx
+++ client/src/pages/MyBlog.jsx
@@ -21,7 +21,7 @@ const MyBlogs = () => {
     <Grid container spacing={3} sx={{ p: 3, height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
       {myBlogs.length > 0 ? (
         myBlogs.map((blog) => (
-          <Grid item xs={12} sm={6} md={4} key={blog._id}>
+          <Grid item xs={12} sm={6} md={4} key={blog.id}>
             <Card>
               <CardMedia component="img" height="140" image={blog.image} alt={blog.title} />
               <CardContent>
@@ -29,8 +29,8 @@ const MyBlogs = () => {
                 <Typography variant="body2" color="text.secondary">{blog.content.substring(0, 100)}...</Typography>
               </CardContent>
               <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
-                <Button size="small" onClick={() => navigate(`/my-blogs/${blog._id}`)}>Read More</Button>
-                <DeleteModal blogId={blog._id} />
+                <Button size="small" onClick={() => navigate(`/my-blogs/${blog.id}`)}>Read More</Button>
+                <DeleteModal blogId={blog.id} />
               </Box>
             </Card>
           </Grid>
