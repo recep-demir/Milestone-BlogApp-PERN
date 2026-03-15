6e85f0c193bfcd1538211e42c27f9fae5b5c7e01
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:35:52 +0100

    refactor: clean up category routes by removing unused variables and improving readability

--- server/src/routes/category.js
+++ server/src/routes/category.js
@@ -1,13 +1,17 @@
-"use strict";
+"use strict"
 
-const router = require("express").Router();
+const router = require('express').Router()
+const category = require('../controllers/category')
+const permissions = require('../middlewares/permissions')
 
-const { list, create, read, update, deletee } = require('../controllers/category');
-const { isAdmin } = require('../middlewares/permissions');
+router.route('/')
+    .get(category.list)
+    .post(permissions.isAdmin, category.create)
 
-
-router.route('/').get(list).post(isAdmin,create);
-router.route('/:id').get(read).put(isAdmin,update).patch(isAdmin,update).delete(isAdmin,deletee)
+router.route('/:id')
+    .get(category.read)
+    .put(permissions.isAdmin, category.update)
+    .patch(permissions.isAdmin, category.update)
+    .delete(permissions.isAdmin, category.remove) // BURASI DEĞİŞTİ
 
 module.exports = router
-
