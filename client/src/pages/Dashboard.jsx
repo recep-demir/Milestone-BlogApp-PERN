9985c460c7e89b7d6392d67895b5be7e7518e6ec
Recep Demir <recep.demir@powercoders.org>
2026-03-16 00:13:58 +0100

    refactor: streamline pagination logic by utilizing URL parameters and improve page change handling

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import useBlogCalls from "../hooks/useBlogCalls";
 import { Container, Grid, Pagination } from "@mui/material";
 import { useSelector } from "react-redux";
 import BlogCard from "../components/blog/BlogCard";
+import { useSearchParams } from "react-router-dom";
 
 const Dashboard = () => {
   const { getBlogs } = useBlogCalls();
   const { blogs } = useSelector((state) => state.blog);
 
-  //!Pagination
-  const [page, setPage] = useState(1);
+  // Pagination durumunu URL'den okuyoruz
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = parseInt(searchParams.get("page") || "1", 10);
+
   const blogsPerPage = 6;
   const totalPages = Math.ceil((blogs?.length || 0) / blogsPerPage);
+  
   const currentBlogs = blogs?.slice(
     (page - 1) * blogsPerPage, 
     page * blogsPerPage
@@ -21,19 +25,17 @@ const Dashboard = () => {
     getBlogs();
   }, []);
 
+  // Sayfa değiştiğinde URL'yi günceller
+  const handlePageChange = (event, value) => {
+    setSearchParams({ page: value });
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Sayfa değişince yumuşakça yukarı kaydır
+  };
+
   return (
     <Container>
-      <Grid container spacing={2} mt={2}>
+      <Grid container spacing={3} mt={2}>
         {currentBlogs?.map((blog) => (
-          <Grid item xs={12} md={6} lg={4} key={blog.id}
-          sx ={{
-          minHeight: "350px",
-          transition: "transform 0.2s ease-in-out",
-          "&:hover": {
-            transform: "scale(1.02)",
-          },
-          }}
-          >
+          <Grid item xs={12} md={6} lg={4} key={blog.id}>
             <BlogCard blog={blog} />
           </Grid>
         ))}
@@ -42,8 +44,9 @@ const Dashboard = () => {
       <Pagination
         count={totalPages}
         page={page}
-        onChange={(event, value) => setPage(value)}
-        sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}
+        onChange={handlePageChange}
+        color="primary"
+        sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}
       />
       <br />
     </Container>
