38c76885738959351e12587c026c057fd6620d25
Recep Demir <recep.demir@powercoders.org>
2026-03-15 22:54:28 +0100

    refactor: clean up formatting and improve BASE_URL assignment in useAxios hook

--- client/src/hooks/useAxios.jsx
+++ client/src/hooks/useAxios.jsx
@@ -1,18 +1,20 @@
 import axios from 'axios';
-import React from 'react'
 import { useSelector } from 'react-redux';
 
 const useAxios = () => {
-    const {token} = useSelector(state => state.auth)
+    const { token } = useSelector(state => state.auth)
+
+    // .env okunamazsa varsayılan olarak lokal backend adresini kullan
+    const BASE_URL = import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000/";
 
     const axiosWithToken = axios.create({
-        baseURL: import.meta.env.VITE_BASE_URL,
+        baseURL: BASE_URL,
         headers: {
             Authorization: `Token ${token}`
-          }
-      });
+        }
+    });
 
-  return {axiosWithToken}
+  return { axiosWithToken }
 }
 
-export default useAxios
+export default useAxios;
