4fbe19ab71dc8d373ca990715026be00f62e5f6a
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:35:39 +0100

    refactor: streamline user controller methods and enhance Swagger documentation

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,104 +1,67 @@
 "use strict"
-
-const User = require("../models/user");
-const CustomError = require("../helpers/customError")
-
+const { User } = require("../models/index");
 
 module.exports = {
-    list:async (req,res)=>{
-
-            /* 
-            #swagger.tags = ['Users']
-            #swagger.summary = 'List Users'
-            #swagger.description = `
-                You can sen query with endpoint for filter[], search[], sort[], page and limit.
-                <ul> Examples usage:
-                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
-                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
-                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
-                    <li>URL/?<b>page=2&limit=1</b></li>
-                </ul>
-            `
+    list: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
         */
-
-            const result = await res.getModelList(User)
-            res.status(200).send({
-                error:false,
-                details:await res.getModelListDetails(User),
-                result
-            })
-
+        const data = await User.findAll();
+        res.status(200).send({
+            error: false,
+            count: data.length,
+            result: data,
+        });
     },
     create: async (req, res) => {
-    /* 
-            #swagger.tags = ['Users']
-            #swagger.summary = 'Create User'
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
         */
-
-    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(req.body.password)) {
-      res.errorStatusCode = 401;
-      throw new Error(
-        "Password must be at least 8 characters long and contain at least one special character and  at least one uppercase character "
-      );
-    }
-    const result = await User.create(req.body);
-
-   
-    res.status(201).send({
-      error: false,
-      result,
-    });
-  },
+        const data = await User.create(req.body);
+        res.status(201).send({
+            error: false,
+            result: data,
+        });
+    },
     read: async (req, res) => {
-    /* 
-            #swagger.tags = ['Users']
-            #swagger.summary = 'Get Single User'
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
         */
-
-    const result = await User.findOne({ _id: req.params.id });
-    res.status(200).send({
-      error: false,
-      result,
-    });
-  },
-
+        const data = await User.findByPk(req.params.id);
+        res.status(200).send({
+            error: false,
+            result: data,
+        });
+    },
     update: async (req, res) => {
         /*
             #swagger.tags = ["Users"]
             #swagger.summary = "Update User"
-            #swagger.parameters['body'] = {
-                in: 'body',
-                required: true,
-                schema: {
-                    "username": "test",
-                    "password": "1234",
-                    "email": "test@site.com",
-                    "firstName": "test",
-                    "lastName": "test",
-                }
-            }
         */
-
-
-    const result = await User.findOneAndUpdate({ _id: req.params.id }, req.body, { runValidators: true, new: true });
-
-        if (!result) throw new CustomError("Update failed, data is not found or already updated", 404);
-
+        const [updatedRows] = await User.update(req.body, {
+            where: { id: req.params.id },
+            individualHooks: true
+        });
+        const data = await User.findByPk(req.params.id);
         res.status(202).send({
             error: false,
-            result
+            result: data,
         });
     },
-  deletee: async (req, res) => {
-    /* 
-        #swagger.tags = ['Users']
-        #swagger.summary = 'Delete User'
-    */
-    const result = await User.deleteOne({ _id: req.params.id })
-    res.status(result.deletedCount ? 204 : 404).send({
-      error: !deletedCount,
-    });
-  }
-
-    
-}
+    remove: async (req, res) => {
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
+        const data = await User.destroy({ where: { id: req.params.id } });
+        if (data) {
+            res.sendStatus(204);
+        } else {
+            res.errorStatusCode = 404;
+            throw new Error('Not Found.');
+        }
+    },
+};
