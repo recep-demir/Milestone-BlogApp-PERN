e3054ea3f4aaae8cc346413d34570fcaafa6c466
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:09:31 +0100

    refactor: migrate Token model from Mongoose to Sequelize with updated schema

--- server/src/models/Token.js
+++ server/src/models/Token.js
@@ -1,23 +1,13 @@
-"use strict"
+"use strict";
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../configs/dbConnection');
 
-const { mongoose } = require('../configs/dbConnection')
-
-const TokenSchema = new mongoose.Schema({
-
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-        index: true,
-    }, 
-
+const Token = sequelize.define('Token', {
     token: {
-        type: String,
-        trim: true,
-        required: true,
-        index: true,
-    }, 
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }
+});
 
-}, { collection: 'tokens', timestamps: true })
-
-module.exports = mongoose.model('Token', TokenSchema)
+module.exports = Token;
