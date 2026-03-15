866fdd01e440a6f1da6526c722f4ed8285d8b630
Recep Demir <recep.demir@powercoders.org>
2026-03-16 00:14:09 +0100

    refactor: update Blog model comments for clarity and consistency

--- server/src/models/Blog.js
+++ server/src/models/Blog.js
@@ -1,5 +1,7 @@
 "use strict";
-const { DataTypes } = require('sequelize');
+// 1. DataTypes'ı doğrudan sequelize kütüphanesinden alıyoruz:
+const { DataTypes } = require('sequelize'); 
+// 2. Kendi oluşturduğumuz bağlantıdan sadece sequelize'ı alıyoruz:
 const { sequelize } = require('../configs/dbConnection');
 
 const Blog = sequelize.define('Blog', {
@@ -18,11 +20,14 @@ const Blog = sequelize.define('Blog', {
         type: DataTypes.BOOLEAN,
         defaultValue: true,
     },
+    likes: {
+        type: DataTypes.ARRAY(DataTypes.INTEGER), // Like atan User ID'lerini tutacak Dizi
+        defaultValue: []
+    },
     countOfVisitors: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.INTEGER, // Okunma sayısını tutacak Sayı
         defaultValue: 0,
     }
-    // NOT: userId ve categoryId alanlarını ilişki kurarken Sequelize otomatik ekleyecek.
 });
 
 module.exports = Blog;
