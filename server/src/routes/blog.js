b612db1ef1ec0948a34570a8683d19a0516cb94e
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:35:49 +0100

    refactor: unify permissions middleware usage in blog routes

--- server/src/routes/blog.js
+++ server/src/routes/blog.js
@@ -2,18 +2,16 @@
 
 const router = require('express').Router()
 const blog = require('../controllers/blog')
-const {isLogin} = require('../middlewares/permissions')
+const permissions = require('../middlewares/permissions')
 
 router.route('/')
-  .get(blog.list)
-  .post(isLogin, blog.create)
+    .get(blog.list)
+    .post(permissions.isLogin, blog.create)
 
 router.route('/:id')
-  .get(blog.read)
-  .put(blog.update)
-  .patch(blog.update)
-  .delete(blog.deletee)
-
-  router.post('/:id/toggle-like', isLogin, blog.toggleLike)
+    .get(blog.read)
+    .put(permissions.isLogin, blog.update)
+    .patch(permissions.isLogin, blog.update)
+    .delete(permissions.isLogin, blog.remove) // BURASI DEĞİŞTİ
 
 module.exports = router
