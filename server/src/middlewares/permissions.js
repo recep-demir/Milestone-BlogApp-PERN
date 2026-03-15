04fbcd11f4f2168cd04ffb5cdea218e6ec77df42
Recep Demir <recep.demir@powercoders.org>
2026-03-15 13:35:46 +0100

    refactor: simplify permissions middleware by removing inactive user checks

--- server/src/middlewares/permissions.js
+++ server/src/middlewares/permissions.js
@@ -1,49 +1,20 @@
 "use strict"
 
-
 module.exports = {
-
     isLogin: (req, res, next) => {
-
-        // any User:
-        if (req.user && req.user.isActive) {
-
+        if (req.user) {
             next()
-
         } else {
-
             res.errorStatusCode = 403
             throw new Error('NoPermission: You must login.')
         }
     },
-
     isAdmin: (req, res, next) => {
-
-        
-        // only Admin:
-        if (req.user && req.user.isActive && req.user.isAdmin) {
-
+        if (req.user && req.user.isAdmin) {
             next()
-
         } else {
-
             res.errorStatusCode = 403
             throw new Error('NoPermission: You must login and to be Admin.')
         }
-    },
-
-    isStaff: (req, res, next) => {
-
-        
-        // only Admin or Staff:
-        if (req.user && req.user.isActive && (req.user.isAdmin || req.user.isStaff)) {
-
-            next()
-
-        } else {
-
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Staff.')
-        }
-    },
+    }
 }
